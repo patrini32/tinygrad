808dbd2e045b604f7472a601cf10c829764b7f18
patrini32 <patrini23@proton.me>
2025-01-04 18:22:01 +0300

    Update ops_amd.py

--- tinygrad/runtime/ops_amd.py
+++ tinygrad/runtime/ops_amd.py
@@ -402,6 +402,7 @@ class PCIIface:
   gpus:list[Any] = []
 
   def __init__(self, dev, dev_id):
+    print("HERE")
     self.dev = dev
 
     if first_dev:=len(PCIIface.gpus) == 0:
@@ -409,7 +410,7 @@ class PCIIface:
       pci_iter = libpciaccess.pci_id_match_iterator_create(None)
       while pcidev:=libpciaccess.pci_device_next(pci_iter):
         if pcidev.contents.vendor_id == 0x1002 and pcidev.contents.device_id == 0x744c: PCIIface.gpus.append(pcidev.contents)
-
+      print(PCIface.gpus)
       # TODO: visible_devices should be handled layer above this?
       visible_devices = [int(x) for x in (getenv('VISIBLE_DEVICES', getenv('HIP_VISIBLE_DEVICES', ''))).split(',') if x.strip()]
       PCIIface.gpus = [PCIIface.gpus[x] for x in visible_devices] if visible_devices else PCIIface.gpus
