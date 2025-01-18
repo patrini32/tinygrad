ef8a5b580122d73785cda78e46c6113d662a6d2d
patrini32 <patrini23@proton.me>
2025-01-18 19:34:14 +0300

    fix

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -239,7 +239,7 @@ class CPUProgram:
     # libgcc_s comes as shared library but compiler-rt is only a bunch of static library archives which we can't directly load, but fortunately
     # it somehow found its way into libSystem on macos (likely because it used __builtin_clear_cache) and libgcc_s is ~always present on linux
     # Using ["name"] instead of .name because otherwise name is getting mangled: https://docs.python.org/3.12/reference/expressions.html#index-5
-    CPUProgram.helper_handle["__clear_cache"](ctypes.c_void_p(mv_address(self.mem)), ctypes.c_void_p(mv_address(self.mem) + len(lib)))
+    #CPUProgram.helper_handle["__clear_cache"](ctypes.c_void_p(mv_address(self.mem)), ctypes.c_void_p(mv_address(self.mem) + len(lib)))
 
     self.fxn = ctypes.CFUNCTYPE(None)(mv_address(self.mem))
 
