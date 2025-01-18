5049458a1f6d8f9e0cb503b5e1fd117f60a4afaf
patrini32 <patrini23@proton.me>
2025-01-18 19:51:00 +0300

    fix

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,5 +1,5 @@
 import platform, tempfile, pathlib, subprocess
-from tinygrad.helpers import cpu_objdump, capstone_flatdump
+from tinygrad.helpers import cpu_objdump, capstone_flatdump, WIN
 from tinygrad.device import Compiled, Compiler, MallocAllocator, CPUProgram
 from tinygrad.runtime.support.elf import jit_loader
 from tinygrad.renderer.cstyle import ClangRenderer
@@ -26,7 +26,7 @@ class ClangJITCompiler(Compiler):
   def compile(self, src:str) -> bytes:
     # -fno-math-errno is required for __builtin_sqrt to become an instruction instead of a function call
     # x18 is a reserved platform register. It is clobbered on context switch in macos and is used to store TEB pointer in windows on arm, don't use it
-    args = ['-march=native', f'--target={platform.machine()}-none-unknown-elf', '-O2', '-fPIC', '-ffreestanding', '-fno-math-errno', '-nostdlib']
+    args = ['-march=native', (f'--target=x86_64-w64-windows-gnu' if WIN else f'--target={platform.machine()}-none-unknown-elf'), '-O2', '-fPIC', '-ffreestanding', '-fno-math-errno', '-nostdlib']
     arch_args = ['-ffixed-x18'] if platform.machine() == 'arm64' else []
     obj = subprocess.check_output(['clang', '-c', '-x', 'c', *args, *arch_args, '-', '-o', '-'], input=src.encode('utf-8'))
     return jit_loader(obj)
